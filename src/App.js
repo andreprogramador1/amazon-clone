addf6cc0bd4d0190f945a80eb856e4ab470fc86c
andre <andre_epr@outlook.com>
2021-01-11 18:59:37 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      home
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD8cFk_Fkst6LkKIZivkUL-ajrP8U3W-Ik",
+    authDomain: "challenge-d46ba.firebaseapp.com",
+    projectId: "challenge-d46ba",
+    storageBucket: "challenge-d46ba.appspot.com",
+    messagingSenderId: "334555648246",
+    appId: "1:334555648246:web:3715891c8fbc6692039cd7",
+    measurementId: "G-7Z8HVV7V5D"
+  };
