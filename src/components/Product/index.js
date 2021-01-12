0e794a8341f0fa44842a9a72aa57c0bc518a1094
andre <andre_epr@outlook.com>
2021-01-11 21:57:34 -0300

    addtobasket function

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./product.css";
+import { useStateValue } from "../stateProvider";
 
 function Product({ id, title, image, price, rating }) {
+
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating
+      }
+    })
+  }
  
   return (
     <div className="product">
@@ -22,7 +38,7 @@ function Product({ id, title, image, price, rating }) {
 
       <img src={image} alt="" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
