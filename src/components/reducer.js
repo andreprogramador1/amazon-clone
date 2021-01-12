850deb86aae5bc2fa1161a05551b8c5599cd3172
andre <andre_epr@outlook.com>
2021-01-11 21:14:43 -0300

    context api and use reducer

---
+++ src/components/reducer.js
@@ -0,0 +1,17 @@
+export const initialState = {
+  basket: [],
+  user: null
+}
+
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0)
+
+  const reducer = (state, action) => {
+    console.log(action)
+    switch (action.type) {
+      default:
+        return state;
+    }
+  }
+
+  export default reducer
