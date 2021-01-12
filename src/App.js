a29eafc620da5b18ce30965c67b1e81ee74a0f51
andre <andre_epr@outlook.com>
2021-01-12 15:40:15 -0300

    useEffect auth set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Header from './components/Header'
 import Login from './components/Login'
+import { auth } from "./firebase";
+import { useStateValue } from "./components/stateProvider";
 
 function App() {
+  const [{ basket }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    
+    auth.onAuthStateChanged((authUser) => {
+      console.log('THE USER IS >>>', authUser)
+
+      if(authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null,
+        })
+      }
+    })
+
+  }, [])
+
 return (
   <div className="app">
     <Router>
